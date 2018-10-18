00f12c9dd7cedb8c3e673ad813265b4fb63d8248
Arnold <arnold.herod@gmail.com>
2018-10-18 14:05:16 +0700

    Test file

--- test.js
+++ test.js
@@ -1,3 +1,52 @@
+function highestScore (students) {
+    // Code disini
+    var objRes = {}
+    
+    for (var i = 0 ; i < students.length ; i++){
+      var obj = {}
+      if (objRes[students[i]["class"]] === undefined){
+        obj["name"] = students[i]["name"]
+        obj["score"] = students[i]["score"]
+        objRes[students[i]["class"]] = obj
+      }
+        else{
+            if(students[i]["score"] > objRes["score"]){
+              obj["name"] = students[i]["name"]
+              obj["score"] = students[i]["score"]
+              objRes[students[i]["class"]] = obj
+            }
+          }
+        }
+        return objRes
+      }
+
+
+      function graduates (students) {
+        // Code disini
+        var objRes = {}
+        
+        for (var i = 0 ; i < students.length ; i++){
+          var arr = []
+          var obj = {}
+          if (objRes[students[i]["class"]] === undefined && students[i]["score"] > 75){
+            obj["name"] = students[i]["name"]
+            obj["score"] = students[i]["score"]
+            arr.push(obj)
+            objRes[students[i]["class"]] = arr
+          }
+          else{
+            if(students[i]["score"] > 75){
+                  obj["name"] = students[i]["name"]
+                  obj["score"] = students[i]["score"]
+                  objRes[students[i]["class"]].push(obj)
+            }
+          }
+        }
+        return objRes
+    }
+
+
+
 // REGEX
 
 
